ef49d433bd399cf669fb1dea3cf329f7b18f41b4
Sweets <sweets@comfi.es>
2021-07-28 19:15:30 -0700

    Add alpha URL

--- sodium.rb
+++ sodium.rb
@@ -2,8 +2,8 @@ class Sodium < Formula
   desc "Framework window manager for macOS"
   homepage "https://github.com/Sweets/sodium"
   head "https://github.com/Sweets/sodium.git"
-  url ""
-  sha256 ""
+  url "https://github.com/Sweets/sodium/archive/refs/tags/v0.1-alpha.tar.gz"
+  sha256 "8e8d3a33f636f05de7809c2f1d47dbed3a3c470f068c0fe11a80f430b46943f6"
 
   depends_on "lua"
 
