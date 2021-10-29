9bbcc0ca6392f80e75c4a576ff611c1ab76a4964
Sweets <sweets@comfi.es>
2021-10-28 20:34:59 -0400

    Update archive url

--- mxfw.rb
+++ mxfw.rb
@@ -2,7 +2,7 @@ class Mxfw < Formula
   desc "Framework window manager for macOS"
   homepage "https://github.com/Sweets/mxfw"
   head "https://github.com/Sweets/mxfw.git", branch: "main"
-  url "https://github.com/Sweets/sodium/archive/refs/tags/0.1.20211028.tar.gz"
+  url "https://github.com/Sweets/mxfw/archive/refs/tags/0.1.20211028.tar.gz"
   sha256 "577d8971849a273a4a310ca2c5d9325b9fd219d5423592f5dfc5c1b09d26ec48"
 
   depends_on "meson"
